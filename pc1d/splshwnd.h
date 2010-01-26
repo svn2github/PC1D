4fa3aa80be28001c0d657a15b8fcabd7df9456e2
dclugston <dclugston@ab8dc4de-122f-0410-adf9-d6b3a1efb7dd>
2010-01-26 11:25:55 +0000

    These files seem to have been left out somehow.
    git-svn-id: svn://svn.code.sf.net/p/pc1d/code/trunk@7 ab8dc4de-122f-0410-adf9-d6b3a1efb7dd

---
+++ pc1d/splshwnd.h
@@ -0,0 +1,53 @@
+/* PC1D Semiconductor Device Simulator
+Copyright (C) 2003 University of New South Wales
+Authors: Paul A. Basore, Donald A. Clugston
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+/////////////////////////////////////////////////////////////////////////////
+// CSplashWnd window
+
+class CSplashWnd : public CWnd
+{
+// Construction
+public:
+	CSplashWnd();
+
+// Attributes
+public:
+
+// Operations
+public:
+	BOOL DisplaySplashScreen(CWnd* pParentWnd, UINT nID, CCreateContext* pContext = NULL );
+// Implementation
+public:
+	virtual ~CSplashWnd();
+
+protected:
+	// Generated message map functions
+	//{{AFX_MSG(CSplashWnd)
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnKillFocus(CWnd* pNewWnd);
+	afx_msg void OnPaint();
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnTimer(UINT nIDEvent);
+	afx_msg void OnDestroy();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
